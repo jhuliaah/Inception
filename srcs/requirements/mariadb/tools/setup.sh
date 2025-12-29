df5908daeb7fd164165b3476b7ee1a3e7f03c5eb
Jhulia Alves De Carvalho <jhualves@c1r7p2.42sp.org.br>
2025-12-29 20:13:38 -0300

    mariadb almost there

---
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -0,0 +1,10 @@
+
+if [ -f /entrypoint-initdb.d/init.sql.template ]; then
+    # Substitute the variables and generates the final SQL archive
+    envsubst < /entrypoint-initdb.d/init.sql.template > /etc/mysql/init.sql
+    # Removes the template
+    rm /entrypoint-initdb.d/init.sql.template
+fi
+
+# Init the main service (MariaDB) like PID 1
+exec "$@"
